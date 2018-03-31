d474b1d4696e1b02f14682f00cabb3ba7bb18f41
Thiago Adriano <tadriano@band.com.br>
2018-03-31 19:26:32 -0300

    subindo css do botao

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,21 +4,23 @@ import { LedsService } from './services/leds.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'app';
   led: String = 'off';
-  statusLed: String = 'LIGAR';
+  statusLed: String = 'DESLIGADO';
   turn: Boolean = false;
-  constructor(private ledsService: LedsService) {}
+  constructor(private ledsService: LedsService) { }
 
   mamangerLed() {
+    console.log(this.turn);
     this.turn = !this.turn;
-    this.led = this.turn ? 'on' : 'off';
-    this.statusLed = this.turn ? 'DESLIGAR' : 'LIGAR';
-    this.ledsService.get(this.led).subscribe((result) => {
 
-    });
+    this.led = this.turn ? 'on' : 'off';
+    this.statusLed = this.turn ? 'DESLIGADO' : 'LIGADO';
+    // this.ledsService.get(this.led).subscribe((result) => {
+
+    // });
   }
 }
